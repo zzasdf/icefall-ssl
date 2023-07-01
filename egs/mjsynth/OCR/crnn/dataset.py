958f28700f893d4ac4c342106daefae8598d863f
Yifan Yang <yifanyeung@qq.com>
2023-07-01 18:22:10 +0800

    Add train decode crnn dataset utils

---
+++ egs/mjsynth/OCR/crnn/dataset.py
@@ -0,0 +1,153 @@
+# Copyright    2017 Jieru Mei meijieru@gmail.com
+#
+# The MIT License (MIT)
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files
+# (the "Software"), to deal in the Software without restriction,
+# including without limitation the rights to use, copy, modify, merge,
+# publish, distribute, sublicense, and/or sell copies of the Software,
+# and to permit persons to whom the Software is furnished to do so,
+# subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+# DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+# TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+# SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+import random
+import sys
+
+import lmdb
+import numpy as np
+import six
+import torch
+import torchvision.transforms as transforms
+from PIL import Image
+from torch.utils.data import Dataset, sampler
+
+
+class lmdbDataset(Dataset):
+    def __init__(self, root=None, transform=None, target_transform=None):
+        self.env = lmdb.open(
+            root,
+            max_readers=1,
+            readonly=True,
+            lock=False,
+            readahead=False,
+            meminit=False,
+        )
+
+        if not self.env:
+            print("cannot creat lmdb from %s" % (root))
+            sys.exit(0)
+
+        with self.env.begin(write=False) as txn:
+            nSamples = int(txn.get("num-samples"))
+            self.nSamples = nSamples
+
+        self.transform = transform
+        self.target_transform = target_transform
+
+    def __len__(self):
+        return self.nSamples
+
+    def __getitem__(self, index):
+        assert index <= len(self), "index range error"
+        index += 1
+        with self.env.begin(write=False) as txn:
+            img_key = "image-%09d" % index
+            imgbuf = txn.get(img_key)
+
+            buf = six.BytesIO()
+            buf.write(imgbuf)
+            buf.seek(0)
+            try:
+                img = Image.open(buf).convert("L")
+            except IOError:
+                print("Corrupted image for %d" % index)
+                return self[index + 1]
+
+            if self.transform is not None:
+                img = self.transform(img)
+
+            label_key = "label-%09d" % index
+            label = str(txn.get(label_key))
+
+            if self.target_transform is not None:
+                label = self.target_transform(label)
+
+        return (img, label)
+
+
+class resizeNormalize(object):
+    def __init__(self, size, interpolation=Image.BILINEAR):
+        self.size = size
+        self.interpolation = interpolation
+        self.toTensor = transforms.ToTensor()
+
+    def __call__(self, img):
+        img = img.resize(self.size, self.interpolation)
+        img = self.toTensor(img)
+        img.sub_(0.5).div_(0.5)
+        return img
+
+
+class randomSequentialSampler(sampler.Sampler):
+    def __init__(self, data_source, batch_size):
+        self.num_samples = len(data_source)
+        self.batch_size = batch_size
+
+    def __iter__(self):
+        n_batch = len(self) // self.batch_size
+        tail = len(self) % self.batch_size
+        index = torch.LongTensor(len(self)).fill_(0)
+        for i in range(n_batch):
+            random_start = random.randint(0, len(self) - self.batch_size)
+            batch_index = random_start + torch.range(0, self.batch_size - 1)
+            index[i * self.batch_size : (i + 1) * self.batch_size] = batch_index
+        # deal with tail
+        if tail:
+            random_start = random.randint(0, len(self) - self.batch_size)
+            tail_index = random_start + torch.range(0, tail - 1)
+            index[(i + 1) * self.batch_size :] = tail_index
+
+        return iter(index)
+
+    def __len__(self):
+        return self.num_samples
+
+
+class alignCollate(object):
+    def __init__(self, imgH=32, imgW=100, keep_ratio=False, min_ratio=1):
+        self.imgH = imgH
+        self.imgW = imgW
+        self.keep_ratio = keep_ratio
+        self.min_ratio = min_ratio
+
+    def __call__(self, batch):
+        images, labels = zip(*batch)
+
+        imgH = self.imgH
+        imgW = self.imgW
+        if self.keep_ratio:
+            ratios = []
+            for image in images:
+                w, h = image.size
+                ratios.append(w / float(h))
+            ratios.sort()
+            max_ratio = ratios[-1]
+            imgW = int(np.floor(max_ratio * imgH))
+            imgW = max(imgH * self.min_ratio, imgW)  # assure imgH >= imgW
+
+        transform = resizeNormalize((imgW, imgH))
+        images = [transform(image) for image in images]
+        images = torch.cat([t.unsqueeze(0) for t in images], 0)
+
+        return images, labels
---
+++ egs/mjsynth/OCR/crnn/decode.py
@@ -0,0 +1,82 @@
+# Copyright    2023  Xiaomi Corp.        (authors: Yifan Yang)
+#
+# See ../../../../LICENSE for clarification regarding multiple authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import logging
+from pathlib import Path
+
+import crnn
+import dataset
+import jsonlines
+import torch
+import utils
+from PIL import Image
+from torch.autograd import Variable
+
+
+def test():
+    model_path = "./exp/crnn.pt"
+    root_dir = Path("/star-data/yifan/IIIT5K")
+    test_data = Path("/star-data/yifan/IIIT5K/testdata.jsonl")
+    alphabet = "0123456789abcdefghijklmnopqrstuvwxyz"
+
+    model = crnn.CRNN(32, 1, 37, 256)
+    if torch.cuda.is_available():
+        model = model.cuda()
+    logging.info("loading pretrained model from %s" % model_path)
+    model.load_state_dict(torch.load(model_path))
+
+    converter = utils.strLabelConverter(alphabet)
+
+    transformer = dataset.resizeNormalize((100, 32))
+
+    true_cnt = 0
+    tot_cnt = 0
+    with open(test_data) as f:
+        for test_data in jsonlines.Reader(f):
+            label = test_data["GroundTruth"]
+            image = Image.open(root_dir / test_data["ImgName"]).convert("L")
+            image = transformer(image)
+            if torch.cuda.is_available():
+                image = image.cuda()
+            image = image.view(1, *image.size())
+            image = Variable(image)
+
+            model.eval()
+            preds = model(image)
+
+            _, preds = preds.max(2)
+            preds = preds.transpose(1, 0).contiguous().view(-1)
+
+            preds_size = Variable(torch.IntTensor([preds.size(0)]))
+            raw_pred = converter.decode(preds.data, preds_size.data, raw=True)
+            sim_pred = converter.decode(preds.data, preds_size.data, raw=False)
+            logging.info("%-20s => %-20s | %s" % (raw_pred, sim_pred, label.lower()))
+            if sim_pred == label.lower():
+                true_cnt += 1
+            tot_cnt += 1
+
+    logging.info(f"Accuracy: {true_cnt / tot_cnt * 100:.2f}%")
+
+
+def main():
+    formatter = "%(asctime)s %(levelname)s [%(filename)s:%(lineno)d] %(message)s"
+    logging.basicConfig(format=formatter, level=logging.INFO)
+
+    test()
+
+
+if __name__ == "__main__":
+    main()
